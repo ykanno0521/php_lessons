7c9ab49e507a212dbd2cfc4352ce4e4274adfa7a
vagrant <vagrant@localhost.localdomain>
2020-03-17 00:05:34 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+//
+/*
+コメント
+*/
+  echo "hello from the TOP!";
+  echo "hello from the again!";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>Hello world <?php echo "from PHP"; ?></p>
+  
+</body>
+</html>
