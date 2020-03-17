d2304d4408a40eb9ecdf35ea919d7064e125a1d3
vagrant <vagrant@localhost.localdomain>
2020-03-17 17:27:24 +0900

    抽象クラス

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <?php
 
-// よく使う関数
+// 抽象クラス
+abstract class BaseUser {
+  public $name;
+  abstract public function sayHi();
+}
 
-$x = 5.6;
-echo ceil($x); //小数点以下を切り上げる
-echo '<br>';
-echo floor($x); //小数点以下切り捨て
-echo '<br>';
-echo round($x); //四捨五入
-echo '<br>';
-echo rand(1,10); //　ランダムに出力
-echo '<br>';
+class User extends BaseUser {
+  public function sayHi() {
+    echo "hello from User";
+  }
+}
 
+// User::sayHi();
 
-$s1 = "hello";
-$s2 = "ねこ";
-echo strlen($s1); //文字列
-echo '<br>';
-echo mb_strlen($s2); //日本語文字列
-echo '<br>';
-
-printf("%s - %s - %.3f", $s1, $s2, $x );
-
+?>
