83f8a1d11c957c241c36f62311d188c9d04d8d95
vagrant <vagrant@localhost.localdomain>
2020-03-17 15:36:20 +0900

    定数について

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
-$msg = "hello from the TOP!";
-echo $msg;
-var_dump($msg);
+define("My_email", "kanno@gmail.com");
 
+echo My_email;
+// My_email = "hogehoge";
+// echo My_email;
+echo '<br>';
+
+echo "行数を表示：";
+var_dump(__LINE__);
+echo '<br>';
+echo "ファイル名：";
+var_dump(__FILE__);
+echo '<br>';
+echo "ディレクトリ名：";
+var_dump(__DIR__);
 ?>
