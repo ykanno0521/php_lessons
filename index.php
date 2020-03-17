14a0d35b81ecbc791b7cfdea7e3f87290b41d547
vagrant <vagrant@localhost.localdomain>
2020-03-17 15:29:06 +0900

    var_dump

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<?php 
-//
-/*
-コメント
-*/
-  echo "hello from the TOP!";
-  echo "hello from the again!";
+<?php
+
+$msg = "hello from the TOP!";
+echo $msg;
+var_dump($msg);
+
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <p>Hello world <?php echo "from PHP"; ?></p>
-  
-</body>
-</html>
